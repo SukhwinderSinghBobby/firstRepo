5cbb7069fe743a31275806ab3558251123e9fdd3
SukhwinderSingh <47039083+SukhwinderSinghBobby@users.noreply.github.com>
2020-12-17 23:10:46 +0100

    first commit

---
+++ practice.js
@@ -0,0 +1,34 @@
+/* //Exercici 1
+var numero=4;
+document.write(numero);
+var cadena='string';
+document.write(cadena);
+var decimales=12.6;
+document.write(decimales); 
+
+// Exercici 2
+var diasSemana = [
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves", 
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
+diasSemana.forEach((element) => {
+  document.write(element + "<br>");
+});
+*/
+// Exercici 3
+
+var coche = { Marca: "BMW", Modelo: "2k", Fecha: "280219", Cilindrada: "20kg" };
+
+    
+for (const key in coche) {
+        console.log("yes")
+        document.write(coche[key]);
+    
+}   
+//   document.write("<br>"+coche[index]);
+
