383f7866d1798a01d3884332cb34aa10841683b4
Sukhwinder Singh <sukhwinder_bcn@hotmail.es>
2021-02-17 19:52:31 +0100

    lista de editoriales

--- Unidad6/unidad6js.js
+++ Unidad6/unidad6js.js
@@ -2,7 +2,6 @@ let editorialsOptions = [];
 
 let selectorEditorials = document.getElementById("editorialBookSelector");
 
-console.log(newEdit);
 // add editorials into selector of Editorials
 function addEdit(event) {
   event.preventDefault();
@@ -26,67 +25,59 @@ function addEdit(event) {
 // add ubications of libraries from the city
 let ubicationsLibraries = ["Barcelona", "Madrid", "Murcia", "LLeida"];
 let ubicationsList = document.getElementById("ulUbications");
+let counterClicks = 0;
 let addUbication = function (event) {
   event.preventDefault();
+  //dont enable to add more libraries than the array has
+  counterClicks++;
+  if (
+    counterClicks > ubicationsLibraries.length &&
+    ubicationsList.childElementCount == ubicationsLibraries.length
+  )
+    alert("no hay mas direciones");
+  else {
+    let liElement = document.createElement("li");
 
-  let liElement = document.createElement("li");
+    let nIdsListUbcations = 0;
+    if (ubicationsList.hasChildNodes())
+      nIdsListUbcations = ubicationsList.childElementCount;
+    //incrementing ids to avoid id = 0 or same id as lentgh of elements
+    nIdsListUbcations++;
 
-  let nIdsListUbcations = 0;
-  if(ubicationsList.hasChildNodes()) nIdsListUbcations = ubicationsList.childElementCount;
-  //incrementing ids to avoid id = 0 or same id as lentgh of elements
-  nIdsListUbcations++;
-  
-  liElement.setAttribute('id',nIdsListUbcations);
-  
-  let nLibraries = Math.floor(Math.random() * ubicationsLibraries.length);
-  let repeatedLibrary = false;
-  //avoiding repiting same libraries
-  if (ubicationsList.hasChildNodes()) {
-  let listOfAddedLibraries = ubicationsList.children;
-  
-  //document.getElementsByTagName().innerText;
-  
-    for (let index = 0; index <= listOfAddedLibraries.length; index++) {
-      for (let sub_index = 0; sub_index < index; sub_index++) {
-        if (listOfAddedLibraries[index].innerText == ubicationsLibraries[sub_index])
+    liElement.setAttribute("id", nIdsListUbcations);
 
+    let nLibraries = Math.floor(Math.random() * ubicationsLibraries.length);
 
-        
-        {
-          console.log("valor anterior LIBRERIA; "+ubicationsLibraries[nLibraries]);
-          index=2
-subindex =0
-
-prier li posicio 1 texto = madrid == array 0 == bcn
-	  pos 2	texto) == bcn == array 1 == bcn 			
-				  
-          let valorAnterior = nLibraries;
-          //Repeated library ubication, make a new random select
-          while(valorAnterior!=nLibraries){
-            nLibraries = Math.floor(Math.random() * ubicationsLibraries.length);
-          }
-                  
-          console.log("valor nuevo LIBRERIA; "+ubicationsLibraries[nLibraries]);
-        
-          
-        }   
-        
-      }
-        
+    //avoiding repiting same libraries
+    let repetead = false;
+    if (ubicationsList.hasChildNodes()) {
+      let listOfAddedLibraries = ubicationsList.children;
       
+      for (let index = 0; index < listOfAddedLibraries.length; index++   ) {
+        if (
+          listOfAddedLibraries[index].innerText == ubicationsLibraries[nLibraries]) {
+          //listOfAddedLibraries[index].remove();
+          console.log(ubicationsLibraries[nLibraries]);
+         
+          nLibraries = Math.floor(Math.random() * (ubicationsLibraries.length - 0) +0);
+          console.log("nuevoValorRandom" + nLibraries);
+          repetead == true;
+          index=0;
+        }
+      }
+
     }
-   
+      liElement.innerText = ubicationsLibraries[nLibraries];
+      ubicationsList.append(liElement);
+    }
   }
 
-  liElement.innerText = ubicationsLibraries[nLibraries];
-  ubicationsList.append(liElement);
-
-
-};
 // delete ubications of libraries from the city
 let deleteUbication = function (event) {
   event.preventDefault();
-  if(ubicationsLibraries.length > 0 && ubicationsList.childNodes.length>0)
-  ubicationsList.lastChild.remove();
-  else alert('No hi han direccions de libreries!');
+  if (ubicationsList.childNodes.length > 0) {
+    ubicationsList.lastChild.remove();
+    counterClicks--;
+  }
+  else alert("No hi han direccions de libreries!");
 };
